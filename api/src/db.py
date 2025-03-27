31cb24ace6dfb2484dc1f8d350070cf8f37764ad
eddy0604 <62774201+eddy0604@users.noreply.github.com>
2025-03-27 16:33:52 +0900

    2일차 수업 완료 커밋

--- api/src/db.py
+++ api/src/db.py
@@ -14,8 +14,14 @@ engine = create_engine(feature_store_url, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# from sqlalchemy.ext.asyncio import create_async_engine
+# async_engine = create_async_engine(feature_store_url, echo=False)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
 
 def get_db():
     """데이터베이스 세션을 제공하는 함수 (의존성 주입용)"""
+    """api 요청 한번에 세션을 받아가게 됨. api 내부에서 세션을 생성하는게 아님"""
     with SessionLocal() as session:
         yield session
